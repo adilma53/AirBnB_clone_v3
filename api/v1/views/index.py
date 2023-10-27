64c31d4336c215a8969d112b4c0be2b7b23481db
div-styl <gouhmada@gmail.com>
2023-10-27 21:37:06 +0100

    Update!

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+Index view for the API.
+"""
+from api.v1.views import app_views
+from flask import jsonify, request
+from models import storage
+
+
+@app_views.route("/status", methods=["GET"])
+def status():
+    if request.method == "GET":
+        return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats", methods=["GET"])
+def stats():
+    if request.method == "GET":
+        resp = {}
+        PLURALS = {
+            "amenities": "amenities",
+            "cities": "cities",
+            "places": "places",
+            "reviews": "reviews",
+            "states": "states",
+            "users": "users",
+        }
+        for key, value in PLURALS.items():
+            resp[key] = storage.count(value)
+        return jsonify(resp)
