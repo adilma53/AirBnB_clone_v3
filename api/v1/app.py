93a4987bb8806ee5ab67eb291a6fff07a1af17b9
adil mae <itsadilhadid@gmail.com>
2023-10-29 17:52:19 +0100

    trying app.js

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,47 +1,29 @@
 #!/usr/bin/python3
-'''flask app for our api'''
-import os
+""" flask app for our api """
+from os import getenv
 from flask import Flask, jsonify
-from flask_cors import CORS
-
-from models import storage
 from api.v1.views import app_views
+from models import storage
 
-'''set up flask up'''
 app = Flask(__name__)
-app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
-CORS(app, resources={'/*': {'origins': app_host}})
-
-
-@app.teardown_appcontext
-def teardown_flask(exception):
-    '''shutdown engine'''
-    storage.close()
 
 
 @app.errorhandler(404)
-def error_404(error):
-    '''handle 404 error status'''
-    return jsonify(error='Not found'), 404
+def not_found(error):
+    """ handle 404 error status """
+    return jsonify({'error': 'Not found'}), 404
 
 
-@app.errorhandler(400)
-def error_400(error):
-    '''handle 400 error status'''
-    errorMessage = 'Bad request'
-    if isinstance(error, Exception) and hasattr(error, 'description'):
-        errorMessage = error.description
-    return jsonify(error=errorMessage), 400
+@app.teardown_appcontext
+def teardown(exception):
+    """ shutdown engine """
+    storage.close()
 
 
 if __name__ == '__main__':
-    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(
-        host=app_host,
-        port=app_port,
-        threaded=True
-    )
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = getenv('HBNB_API_PORT', 5000)
+    app.run(host=host, port=port, threaded=True)
