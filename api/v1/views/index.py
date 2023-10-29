0b9fe632f36c74daec91f4293cf86221a04a2149
adil mae <itsadilhadid@gmail.com>
2023-10-29 19:31:56 +0100

    places

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,47 +1,24 @@
 #!/usr/bin/python3
-"""
-Index view for the API.
-"""
+""" index view """
+from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
-@app_views.route("/status", strict_slashes=False, methods=["GET"])
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """
-    Handler function for the /status endpoint.
-
-    Returns:
-        A JSON response with the status "OK".
-    """
-    return {
-        "status": "OK",
-    }
+    """ get JSON """
+    return jsonify(status="OK")
 
 
-@app_views.route("/stats", strict_slashes=False, methods=["GET"])
-def stats():
-    amenities = storage.count(Amenity)
-    cities = storage.count(City)
-    places = storage.count(Place)
-    reviews = storage.count(Review)
-    states = storage.count(State)
-    users = storage.count(User)
-    return {
-        "amenities": amenities,
-        "cities": cities,
-        "places": places,
-        "reviews": reviews,
-        "states": states,
-        "users": users,
-    }
-
-
-if __name__ == "__main__":
-    pass
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
