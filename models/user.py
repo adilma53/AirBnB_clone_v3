c51b303588a0ab7c6818009a036eda8a85d2f412
adil mae <itsadilhadid@gmail.com>
2023-10-29 17:36:52 +0100

    trying to fix user model

--- models/user.py
+++ models/user.py
@@ -1,35 +1,30 @@
 #!/usr/bin/python3
 """This is the user class"""
-import models
 
-from models.base_model import BaseModel
-from models.base_model import Base, BaseModel
+import models
+from models.base_model import BaseModel, Base
 from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
-storage_type = getenv("HBNB_TYPE_STORAGE")
-
 
 class User(BaseModel, Base):
-    """User class inherits from BaseModel
-    Attributes:
-        email (str): email address
-        password (str): password
-        first_name (str): first name
-        last_name (str): last name
-    """
-
-    __tablename__ = "users"
-    if storage_type == "db":
+    """user model"""
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", cascade="all, delete" ,backref="user")
-        review = relationship("Review", cascade="all, delete" ,backref="user")
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
