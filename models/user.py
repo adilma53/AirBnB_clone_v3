db91f495ace9cc44dd5896b8d8859449456fb79d
adil mae <itsadilhadid@gmail.com>
2023-10-29 17:47:28 +0100

    get user back

--- models/user.py
+++ models/user.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 """This is the user class"""
-
 import models
-from models.base_model import BaseModel, Base
+
+from models.base_model import BaseModel
+from models.base_model import Base, BaseModel
 from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+storage_type = getenv("HBNB_TYPE_STORAGE")
+
 
 class User(BaseModel, Base):
-    """user model"""
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    """User class inherits from BaseModel
+    Attributes:
+        email (str): email address
+        password (str): password
+        first_name (str): first name
+        last_name (str): last name
+    """
+
+    __tablename__ = "users"
+    if storage_type == "db":
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+        places = relationship("Place", cascade="all, delete" ,backref="user")
+        review = relationship("Review", cascade="all, delete" ,backref="user")
     else:
         email = ""
         password = ""
         first_name = ""
         last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
