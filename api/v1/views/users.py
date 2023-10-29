5c25ebfd2465d6c0d453e726e424f52ddba5a0f6
adil mae <itsadilhadid@gmail.com>
2023-10-29 20:24:53 +0100

    trying to fix task 9

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -5,23 +5,29 @@ from api.v1.views import app_views
 from models import storage
 from models.user import User
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
-def get_users():
+def users():
     """get all users in storage"""
+
     userObjs = storage.all(User)
     return jsonify([obj.to_dict() for obj in userObjs.values()])
 
+
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def get_user(user_id):
+def single_user(user_id):
     """get a specific user by id"""
+
     userObj = storage.get(User, user_id)
     if not userObj:
         abort(404)
     return jsonify(userObj.to_dict())
 
+
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def delete_user(user_id):
+def del_user(user_id):
     """delete a specific user by id"""
+
     userObj = storage.get(User, user_id)
     if not userObj:
         abort(404)
@@ -29,32 +35,40 @@ def delete_user(user_id):
     storage.save()
     return make_response(jsonify({}), 200)
 
+
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
-def create_user():
-    """create user object and return it if successful"""
-    data = request.get_json()
-    if not data:
+def post_user():
+    """create user object and return it if successfull"""
+
+    newObj = request.get_json()
+    if not newObj:
         abort(400, "Not a JSON")
-    if 'email' not in data:
+    if 'email' not in newObj:
         abort(400, "Missing email")
-    if 'password' not in data:
+    if 'password' not in newObj:
         abort(400, 'Missing password')
-    obj = User(**data)
+
+    obj = User(**newObj)
     storage.new(obj)
     storage.save()
     return make_response(jsonify(obj.to_dict()), 201)
 
+
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-def update_user(user_id):
-    """update user object and return it if successful"""
+def put_user(user_id):
+    """update user object and return it if successfull"""
+
     userObj = storage.get(User, user_id)
     if not userObj:
         abort(404)
-    data = request.get_json()
-    if not data:
+
+    req = request.get_json()
+    if not req:
         abort(400, "Not a JSON")
-    for k, v in data.items():
+
+    for k, v in req.items():
         if k not in ['id', 'email', 'created_at', 'updated_at']:
             setattr(userObj, k, v)
+
     storage.save()
     return make_response(jsonify(userObj.to_dict()), 200)
